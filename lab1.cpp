d9ccbecb5cd7056e98ad14661bfed43a8682a817
ilyanikoniko <iliya05163@gmail.com>
2025-03-31 20:41:00 +0300

    поменял реализацию согласно варианту

--- lab1.cpp
+++ lab1.cpp
@@ -1,7 +1,12 @@
+#include <windows.h>
 #include <iostream>
 #include <random>
+#include <algorithm>
+#include <chrono>
+
 
 using namespace std;
+
 class Binary {
     private:
 
@@ -60,6 +65,7 @@ class Binary {
 
     bool contains(Node* node, int key) const {
         if (node == nullptr) {
+               
             return false;
         }
         if (key < node->key) {
@@ -67,6 +73,7 @@ class Binary {
         } else if (key > node->key) {
             return contains(node->right, key);
         }
+        
         return true; 
     }
 
@@ -79,9 +86,6 @@ class Binary {
         } else if (key > node->key) {
             return erase(node->right, key);
         } else {
-            if (node->left == nullptr && node->right == nullptr) {
-                delete node;
-                node = nullptr;
             if (node->left == nullptr) {
                 Node* temp = node->right;
                 delete node;
@@ -101,8 +105,22 @@ class Binary {
             return true;
         }
     }
-}
 
+    void inorderTraversal(Node* node, vector<int>& elements) const {
+        if (node == nullptr) return;
+        inorderTraversal(node->left, elements);
+        elements.push_back(node->key);
+        inorderTraversal(node->right, elements);
+    }
+
+    void findIntersection(Node* node, const Binary& other, vector<int>& result) const {
+        if (node == nullptr) return;
+        findIntersection(node->left, other, result);
+        if (other.contains(node->key)) {
+            result.push_back(node->key);
+        }
+        findIntersection(node->right, other, result);
+    }
 
     public:
     Binary() : root(nullptr) {}
@@ -117,6 +135,13 @@ class Binary {
             root = copy(other.root);  
         }
         return *this;  
+    }
+
+    // Функция-генератор псевдослучайных чисел
+    size_t lcg(){
+        static size_t x = 0;
+        x = (1021*x+24631) % 116640;
+        return x;
     }
 
     void print() const {
@@ -135,4 +160,180 @@ class Binary {
     bool erase(int key) {
         return erase(root, key);
     }
+    
+    void inorderTraversal(vector<int>& elements) const {
+        inorderTraversal(root, elements);
+    }
+
+    void findIntersection(const Binary& other, vector<int>& result) const {
+        findIntersection(root, other, result);
+    }
 };
+
+vector<int> getUnion(const Binary& tree1, const Binary& tree2) {
+    vector<int> result;
+
+    tree1.inorderTraversal(result);
+    tree2.inorderTraversal(result);
+
+    sort(result.begin(), result.end());               
+    result.erase(unique(result.begin(), result.end()), result.end()); 
+
+    return result;
+}
+
+// Функция для пересечения двух деревьев
+vector<int> getIntersection(const Binary& tree1, const Binary& tree2) {
+    vector<int> result;
+    tree1.findIntersection(tree2, result);
+    return result;
+}
+
+void random_numbers(Binary& tree, size_t count) {
+    for (size_t i = 0; i < count; ++i) {
+        int randomNumber = tree.lcg(); 
+        tree.insert(randomNumber); 
+    }
+}
+
+void filling_task(int size) {
+    cout << "Size:" << size << endl;
+    double time = 0;
+    double time1 = 0;
+    for (int i = 0; i < 100; i++) {
+        Binary tree;
+        vector<int> vec;
+        auto begin = std::chrono::steady_clock::now();
+        for (int i = 0; i < size; i++) {
+            tree.insert(tree.lcg());
+        }
+        auto end = std::chrono::steady_clock::now();
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        time += elapsed_ms.count();
+
+        auto begin1 = std::chrono::steady_clock::now();
+        for (int i = 0; i < size; i++) {
+            vec.push_back(rand());
+        }
+        auto end1 = std::chrono::steady_clock::now();
+        auto elapsed_ms1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - begin1);
+        time1 += elapsed_ms1.count()*1000;
+    }
+    cout << "Time to fill the binary tree:" << (time / 100) << " microseconds" << endl;
+    cout << "Vector filling time:" << (time1 / 100) << " microseconds" << endl;
+}
+
+void search_time(int size)
+{
+    cout << "Size:" << size << endl;
+    double time = 0;
+    double time1 = 0;
+    for (int i = 0; i < 1000; i++) {
+        Binary tree;
+        vector<int> vec;
+        for (int i = 0; i < size; i++) {
+            tree.insert(tree.lcg());
+        }
+        int random_number = rand();
+        auto begin = std::chrono::steady_clock::now();
+        tree.contains(random_number);
+        auto end = std::chrono::steady_clock::now();
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        time += elapsed_ms.count();
+        for (int i = 0; i < size; i++) {
+            vec.push_back(rand());
+        }
+        int random_number1 = rand();
+        auto begin1 = std::chrono::steady_clock::now();
+        auto a = find(vec.begin(), vec.end(), random_number1);
+        auto end1 = std::chrono::steady_clock::now();
+        auto elapsed_ms1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - begin1);
+        time1 += elapsed_ms1.count();
+    }
+    cout << "Binary tree number search time:" << (time / 100) << " microseconds" << endl;
+    cout << "Vector number search time:" << (time1 / 100) << " microseconds" << endl;
+}
+void adding_and_deleting_time(int size) {
+    cout << "Size:" << size << endl;
+    double time = 0;
+    double time1 = 0;
+    for (int i = 0; i < 1000; i++) {
+        Binary tree;
+        vector<int> vec;
+        for (int i = 0; i < size; i++) {
+            tree.insert(tree.lcg());
+        }
+        int random_number = rand();
+        int random_number_delete = rand();
+        auto begin = std::chrono::steady_clock::now();
+        tree.insert(random_number);
+        tree.erase(random_number_delete);
+        auto end = std::chrono::steady_clock::now();
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        time += elapsed_ms.count();
+        for (int i = 0; i < size; i++) {
+            vec.push_back(rand());
+        }
+        int random_number1 = rand();
+        int random_number_delete1 = rand();
+        auto begin1 = std::chrono::steady_clock::now();
+        vec.push_back(random_number1);
+        vec.erase(remove(vec.begin(), vec.end(), random_number_delete1), vec.end());
+        auto end1 = std::chrono::steady_clock::now();
+        auto elapsed_ms1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - begin1);
+        time1 += elapsed_ms1.count();
+    }
+    cout << "Time of adding and removing numbers of a binary tree:" << (time / 100) << " nanoseconds" << endl;
+    cout << "Time to add and remove a vector number:" << (time1 / 100) << " nanoseconds" << endl;
+}
+
+int main() {
+   setlocale(LC_ALL, "ru");
+   SetConsoleCP(1251);
+   SetConsoleOutputCP(1251);
+   Binary tree;
+   Binary tree1;
+   Binary tree2;
+   tree2.insert(30);
+   tree2.insert(20);
+   tree.insert(50);
+   tree.insert(30);
+   tree.insert(20);
+   tree.print();
+   tree.insert(20);
+   tree.print();
+   tree.contains(10);
+   tree.erase(20);
+   tree.print();
+   tree.insert(20);
+   tree1.insert(5);
+   tree1.insert(10);
+   vector<int> v = getUnion(tree,tree1);
+   cout<<"The union of trees:"<<endl;
+   for(int i=0; i<v.size(); ++i)
+   {
+    cout<<v[i]<<endl;
+   }
+   vector<int> v1 = getIntersection(tree,tree2);
+   cout<<"The intersection of trees:"<<endl;
+   for(int i=0; i<v1.size(); ++i)
+   {
+    cout<<v1[i]<<endl;
+   }
+
+   // Для каждого размера контейнера (1000, 10000, 100000)
+   for (int size : {1000, 10000, 100000}) {
+    cout << "Size of contanier: " << size << endl;
+    
+    // Замер времени для заполнения
+    filling_task(size);
+    
+    // Замер времени для поиска
+    search_time(size);
+    
+    // Замер времени для добавления и удаления
+    adding_and_deleting_time(size);
+    
+    cout << endl;
+    }
+}
--- lab2.cpp
+++ lab2.cpp
@@ -1,33 +1,76 @@
 #include <iostream>
 #include <cstdlib>  
 #include <ctime>
-#include <optional>
 #include <string>
 #include <vector>
-#include <utility>  // для std::pair
 
+
+
+using namespace std;
+
+template <typename K, typename T>
 class Hash_table {
 private:
-    struct Bucket {
-        std::optional<std::pair<int, std::string>> data;  // Пара ключ-значение или пусто
-        bool isDeleted = false;  // Флаг для удалённых элементов
+    struct Node {
+        K key;
+        T value;
+        Node* next;
+        Node(K k, const T& v) : key(k), value(v), next(nullptr) {}
     };
 
-    size_t table_size;  
-    Bucket* table;  // Массив бакетов
+    Node** table;// массив указателей на Node
+    size_t table_size;
+    size_t table_capacity; 
+
+    // Хэш-функция 
+    size_t hash_Function(const K key) const {
+        const uint64_t a = 2654435761;// случайная нечётная константа,
+        const uint64_t w = 32;// размер машинного слова в битах
+        uint64_t key64 = static_cast<uint64_t>(key);//кдюч приводится к 64-битному типу, чтобы избежать переполнения при умножении.
+        uint64_t hash = (key64 * a) % (1ULL << w);//(k * a) mod 2^w
+        return hash >> (w - static_cast<uint64_t>(log2(table_size)));//битовый сдвиг на (w - l) бит
+    }
+
 public:
     //Конструктор для пустой таблицы
-    Hash_table(size_t size = 10) : table_size(size), table(new Bucket[size]) {}
+    Hash_table(size_t capacity = 16) : table_size(0), table_capacity(capacity) {
+        table = new Node*[table_capacity]();
+    }
+
+    // Конструктор, заполняющий хэш таблицу случайными значениями
+    Hash_table(size_t capacity, size_t num) : table_capacity(capacity), table_size(0) {
+        table = new Node*[table_capacity]();
+        for (size_t i = 0; i < num; ++i) {
+            K key = static_cast<K>(rand());
+            T value = static_cast<T>(rand());
+            insert(key, value);
+        }
+    }
 
     // Конструктор копирования
-    Hash_table(const Hash_table& other): table_size(other.table_size), table(new Bucket[other.table_size]){
-        for (size_t i = 0; i < table_size; ++i) {
-            table[i] = other.table[i];  
+    Hash_table(const Hash_table& other): table_size(other.table_size), table_capacity(other.table_capacity){
+        table = new Node*[table_capacity]();
+        for (size_t i = 0; i < capacity; ++i) {
+            Node* current = other.table[i];
+            while (current != nullptr) {
+                insert(current->key, current->value);
+                current = current->next;
+            }
         }
     }
 
     // Деструктор
     Hash_table() {
+        for (size_t i = 0; i < capacity; ++i) {
+            Node* current = table[i];
+            while (current != nullptr) {
+                Node* next = current->next;
+                delete current;
+                current = next;
+            }
+            table[i] = nullptr;
+        }
+        size = 0;
         delete[] table;
     }
 };
